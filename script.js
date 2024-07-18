c28e51ff3a37d2777b748b4f72ef29bdab2faaf0
pumin-buch <buchpumin@gmail.com>
2024-07-18 16:49:38 +0200

    pushing

---
+++ script.js
@@ -0,0 +1,22 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function backspace() {
+    let display = document.getElementById('display').value;
+    document.getElementById('display').value = display.substring(0, display.length - 1);
+  }
+  
+  function calculate() {
+    try {
+      let result = eval(document.getElementById('display').value);
+      document.getElementById('display').value = result;
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
+  
